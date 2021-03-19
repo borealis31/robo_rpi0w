4a36133b4f7578ee292e5d2cc182c79726793194
borealis31 <jakeb.chouinard@gmail.com>
2021-03-18 21:11:36 -0400

    Add strafe functionality

--- testingCode/testingcode.py
+++ testingCode/testingcode.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 from evdev import InputDevice, categorize, ecodes
 from gpiozero import Motor
 import time
@@ -16,18 +15,45 @@ for event in remote.read_loop():
 	if event.type == ecodes.EV_KEY:
 		print(event.code)
 		print(event.value)
-	if event.code == 128:
+	if event.code == 128: #KEY_STOP END PROGRAM
 		break
-	if event.code == 3 and (event.value == 1 or event.value == 2):
+	if event.code == 3 and (event.value == 1 or event.value == 2): #KEY_2 FORWARD
 		asMotor.forward(1)
 		apMotor.forward(1)
 		fsMotor.forward(1)
 		fpMotor.forward(1)
-	elif event.code == 3 and event.value == 0:
+	elif event.code == 3 and event.value == 0: #KEY_2 STOP
 		asMotor.forward(0)
 		apMotor.forward(0)
 		fsMotor.forward(0)
 		fpMotor.forward(0)
-#tM.forward(1)
-#time.sleep(2)
-#tM.forward(0)
+	elif event.code == 9 and (event.value == 1 or event.value == 2): #KEY_8 BACKWARD
+		asMotor.backward(1)
+		apMotor.backward(1)
+		fsMotor.backward(1)
+		fpMotor.backward(1)
+	elif event.code == 9 and event.value == 0: #KEY_8 STOP
+		asMotor.backward(0)
+		apMotor.backward(0)
+		fsMotor.backward(0)
+		fpMotor.backward(0)
+	elif event.code == 5 and (event.value == 1 or event.value == 2): #KEY_5 STRAFE LEFT
+		asMotor.forward(1)
+		apMotor.backward(1)
+		fsMotor.backward(1)
+		fpMotor.forward(1)
+	elif event.code == 5 and event.value == 0: #KEY_5 STOP
+		asMotor.forward(0)
+		apMotor.backward(0)
+		fsMotor.backward(0)
+		fpMotor.forward(0)
+	elif event.code == 7 and (event.value == 1 or event.value == 2): #KEY_7 STRAFE RIGHT
+		asMotor.backward(1)
+		apMotor.forward(1)
+		fsMotor.forward(1)
+		fpMotor.backward(1)
+	elif event.code == 7 and event.value == 0: #KEY_7 STOP
+		asMotor.backward(0)
+		apMotor.forward(0)
+		fsMotor.forward(0)
+		fpMotor.backward(0)
